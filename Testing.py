5bc86e28a69bb6597a4db856a1a75237e047c2e4
Pratik Devikar <devikar.p@husky.neu.edu>
2018-10-21 15:48:19 -0400

    Documentation

--- Testing.py
+++ Testing.py
@@ -1,9 +1,11 @@
-#THIS IS THE TESTING FILE. IT WILL GO ON THE SERVER.
+# THIS IS THE TESTING FILE. IT WILL GO ON THE SERVER.
 
 
-classes = ['Board', 'Dust Pan', 'Fire_Extnguisher','Rubber Glove', 'Apple', 'BackPack', 'Bowl', 'Cap', 'Cell Phone', 'Chair', 'Fan', 'Flashlight', 'Football', 'Glasses', 'Guitar','Laptop', 'Lightbulb', 'Mug', 'Pen', 'Plier', 'Shoe', 'Stapler', 'Wall Clock', 'Water Bottle']
-
-
+classes = ['Board', 'Dust Pan', 'Fire_Extnguisher', 'Rubber Glove', 'Apple',
+           'BackPack', 'Bowl', 'Cap', 'Cell Phone', 'Chair', 'Fan',
+           'Flashlight', 'Football', 'Glasses', 'Guitar', 'Laptop',
+           'Lightbulb', 'Mug', 'Pen', 'Plier', 'Shoe', 'Stapler',
+           'Wall Clock', 'Water Bottle']
 
 import os
 import pickle
@@ -15,24 +17,21 @@ import re
 
 from tensorflow.python.platform import gfile
 
-#Rescale the testing image to 100x100
+# Rescale the testing image to 100x100
 # image_data = gfile.FastGFile('football.jpeg', 'rb').read()
 # print(image_data)
 
 # image_name = ['football.jpeg']
 
-
-
-
 model_dir = 'imagenet'
 images_dir = 'Testing_images/'
-list_images = [images_dir+f for f in os.listdir(images_dir) if re.search('jpeg|JPEG', f)]
-# print(list_images)
+list_images = [images_dir + f for f in os.listdir(images_dir) if
+               re.search('jpeg|JPEG', f)]
 
 
 def create_graph():
     with gfile.FastGFile(os.path.join(
-    model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
+            model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
         _ = tf.import_graph_def(graph_def, name='')
@@ -54,7 +53,6 @@ def extract_features(list_images):
         # if (ind % 100 == 0):
         #     print('Processing %s...' % (image))
 
-
         image_data = gfile.FastGFile(image, 'rb').read()
         predictions = sess.run(next_to_last_tensor,
                                {'DecodeJpeg/contents:0': image_data})
@@ -63,68 +61,40 @@ def extract_features(list_images):
 
     return features, labels
 
-
-
 import time
+
 start_time = time.time()
 
+features, labels = extract_features(list_images)
 
-features,labels = extract_features(list_images)
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(features,
+                                                                     labels,
+                                                                     test_size=0.99)
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(features, labels, test_size=0.99)
+loaded_model2 = pickle.load(open('SVC_Classifier_Probabilities.sav', 'rb'))
+loaded_model1 = pickle.load(open('SVC_Classifier.sav', 'rb'))
 
-# print(X_test, y_test)
-
-
-
-loaded_model2 =  pickle.load(open('SVC_Classifier_Probabilities.sav', 'rb'))
-loaded_model1 =  pickle.load(open('SVC_Classifier.sav', 'rb'))
-
-# image_data = gfile.FastGFile('football.jpeg', 'rb').read()
-# print(image_data.dtype)
 y_pred = loaded_model1.predict(X_test)
 y_pred2 = loaded_model2.predict_proba(X_test)
-# print(y_pred)
+
 
 print("Highest probability is for :", y_pred[0])
-# print(y_pred2)
-print()
-
-# print(y_pred2[0][0])
-
 print('Top 3 Probabilities are')
-
 print(y_pred2)
-
-
-# print(y_pred2[0][23])
-
-# y_pred2 = y_pred2.ravel()
-# y_pred2 = y_pred2.flatten()
-
-# print(y_pred2)
-
-# print(y_pred2.shape)
-
-# print y_pred2
-
 
 final_y_pred = []
 
-for i in range (24):
+for i in range(24):
     final_y_pred.append(y_pred2[0][i])
 
 print(final_y_pred)
 print(classes)
 
 
-# print(classes[12])
-
 for i in range(3):
     index = final_y_pred.index(max(final_y_pred))
     # print(index)
     print(classes[index], final_y_pred[index])
     final_y_pred.pop(index)
-
 
 print("--- %s seconds ---" % (time.time() - start_time))
--- create features.py
+++ create features.py
@@ -1,4 +1,4 @@
-#THIS FILE CREATES  FEATURE AND LABEL FILES.
+# THIS FILE CREATES  FEATURE AND LABEL FILES.
 
 
 import os
@@ -6,7 +6,7 @@ import re
 
 import tensorflow as tf
 import tensorflow.python.platform
-from  tensorflow.python.platform import gfile
+from tensorflow.python.platform import gfile
 import numpy as np
 import pandas as pd
 import sklearn
@@ -16,24 +16,24 @@ from sklearn.svm import SVC, LinearSVC
 import matplotlib.pyplot as plt
 import pickle
 
-
 model_dir = 'imagenet'
 images_dir = 'images/'
-list_images = [images_dir+f for f in os.listdir(images_dir) if re.search('jpeg|JPEG|jpg|JPG', f)]
+list_images = [images_dir + f for f in os.listdir(images_dir) if
+               re.search('jpeg|JPEG|jpg|JPG', f)]
 print(list_images)
-# print(len(list_images))
 
-#To use TensorFlow, you should define a graph that represents the description of computations. Then these computations will be executed within what is called sessions
 
+# To use TensorFlow, you should define a graph that represents the
+# description of computations. Then these computations will be executed
+# within what is called sessions
 
 
 def create_graph():
     with gfile.FastGFile(os.path.join(
-    model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
+            model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
         _ = tf.import_graph_def(graph_def, name='')
-
 
 
 def extract_features(list_images):
@@ -47,11 +47,6 @@ def extract_features(list_images):
         next_to_last_tensor = sess.graph.get_tensor_by_name('pool_3:0')
 
     for ind, image in enumerate(list_images):
-        # print(ind)
-
-        # if (ind % 100 == 0):
-        #     print('Processing %s...' % (image))
-
 
         image_data = gfile.FastGFile(image, 'rb').read()
         predictions = sess.run(next_to_last_tensor,
@@ -61,36 +56,19 @@ def extract_features(list_images):
 
     return features, labels
 
-features,labels = extract_features(list_images)
 
-pickle.dump(features, open('features', 'wb'))
-pickle.dump(labels, open('labels', 'wb'))
-
-features = pickle.load(open('features'))
-labels = pickle.load(open('labels'))
-
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(features, labels, test_size=0.2, random_state=42)
-# print(X_train, y_train)
-
-clf = LinearSVC(C=1.0, loss='l2', penalty='l2',multi_class='ovr')
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-
-
-
-
-def plot_confusion_matrix(y_true,y_pred):
-    cm_array = confusion_matrix(y_true,y_pred)
+def plot_confusion_matrix(y_true, y_pred):
+    cm_array = confusion_matrix(y_true, y_pred)
     true_labels = np.unique(y_true)
     pred_labels = np.unique(y_pred)
-    plt.imshow(cm_array[:-1,:-1], interpolation='nearest', cmap=plt.cm.Blues)
+    plt.imshow(cm_array[:-1, :-1], interpolation='nearest', cmap=plt.cm.Blues)
     plt.title("Confusion matrix", fontsize=16)
     cbar = plt.colorbar(fraction=0.046, pad=0.04)
     cbar.set_label('Number of images', rotation=270, labelpad=30, fontsize=12)
     xtick_marks = np.arange(len(true_labels))
     ytick_marks = np.arange(len(pred_labels))
     plt.xticks(xtick_marks, true_labels, rotation=90)
-    plt.yticks(ytick_marks,pred_labels)
+    plt.yticks(ytick_marks, pred_labels)
     plt.tight_layout()
     plt.ylabel('True label', fontsize=14)
     plt.xlabel('Predicted label', fontsize=14)
@@ -101,8 +79,26 @@ def plot_confusion_matrix(y_true,y_pred):
     plt.show()
 
 
+def main():
+    features, labels = extract_features(list_images)
+    pickle.dump(features, open('features', 'wb'))
+    pickle.dump(labels, open('labels', 'wb'))
+
+    features = pickle.load(open('features'))
+    labels = pickle.load(open('labels'))
+
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+        features,
+        labels,
+        test_size=0.2,
+        random_state=42)
+
+    clf = LinearSVC(C=1.0, loss='l2', penalty='l2', multi_class='ovr')
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print("Accuracy: {0:0.1f}%\n".format(accuracy_score(y_test, y_pred) * 100))
+    plot_confusion_matrix(y_test, y_pred)
 
 
-# print("Accuracy: {0:0.1f}%\n".format(accuracy_score(y_test,y_pred)*100))
-# plot_confusion_matrix(y_test,y_pred)
-
+if __name__=="__main__":
+    main()
